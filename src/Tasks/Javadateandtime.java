226a65761e55fc8a9b2c294f2f97a09a43a2dd7a
Bartosz Matras <bartosz.matras38@gmail.com>
2021-08-10 19:40:45 +0200

    add two files

---
+++ src/Tasks/Javadateandtime.java
@@ -0,0 +1,7 @@
+package Tasks;
+
+public class Javadateandtime {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Tasks/Javainttostring.java
+++ src/Tasks/Javainttostring.java
@@ -1,7 +1,22 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Javainttostring {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
 
+        if (-100 <= n && n <= 100){
+            try{
+                String s = Integer.toString(n);
+                System.out.println("Good job");
+            }catch (Exception e){
+                System.out.println("Wrong answer");
+            }
+        }else{
+            System.out.println("Wrong answer");
+        }
     }
 }
